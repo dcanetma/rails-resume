9877d23e1938138df8ebfcf1d4d6735717927d8c
Daniel Canet <dcanetma@gmail.com>
2022-06-06 19:14:03 +0200

    chore(projects): set right values to permit clause

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
     before_action :set_company
 
     def create
-        @company.projects.create! params.required(:project).permit(:reference) 
+        @company.projects.create! params.required(:project).permit(:reference, :title, :url, :status, :period) 
         redirect_to @company
     end
 
