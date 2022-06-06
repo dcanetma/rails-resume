a13560d96debfedac10324536e896afccb250963
Daniel Canet <dcanetma@gmail.com>
2022-06-06 19:08:43 +0200

    chore: add turbo stream for companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,5 @@ class Company < ApplicationRecord
     validates_presence_of :name
     has_rich_text :description
     has_rich_text :notes
-    has_many :projects
+    has_many :projects, dependent: :destroy 
 end
